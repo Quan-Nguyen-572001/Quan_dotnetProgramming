7e0fed1ff097ca042589086a4dbb8dd3b32d492d
Quan-Nguyen-572001 <minhquannguyen01@gmail.com>
2023-03-14 20:39:14 +0900

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Hi Quan");
-            Console.Write("Hi Dung");
+            // prob1
+            Console.Write("input number: ");
+            double inch = Convert.ToDouble(Console.ReadLine());
+            double cm = inch * 2.54;
+            Console.WriteLine("{0} inch la {1}cm.", inch, cm);
         }
     }
 }
